b03fa98c0d98b59d8c7c2c5aa13260a6f8508a0b
diablouma <mh732@hotmail.com>
2021-05-14 01:19:21 +0200

    Fills shoe list dynamically

--- app/src/main/java/com/udacity/shoestore/screens/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoelist/ShoeListFragment.kt
@@ -2,14 +2,22 @@ package com.udacity.shoestore.screens.shoelist
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ShoeItemWithinListBinding
 import com.udacity.shoestore.databinding.ShoeListFragmentBinding
 
 class ShoeListFragment : Fragment() {
+
+    private lateinit var viewModel: ShoeListViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,6 +27,22 @@ class ShoeListFragment : Fragment() {
             DataBindingUtil.inflate(inflater, R.layout.shoe_list_fragment, container, false)
 
         setHasOptionsMenu(true)
+
+        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
+        val shoeListLayout = binding.shoeListLinearLayout
+
+        viewModel.shoeList.value?.forEach { shoe ->
+            val shoeItemWithinListBinding: ShoeItemWithinListBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.shoe_item_within_list,
+                shoeListLayout,
+                true
+            )
+
+            shoeItemWithinListBinding.shoeNameText.text = shoe.name
+            shoeItemWithinListBinding.shoeDescriptionText.text = shoe.description
+            shoeItemWithinListBinding.shoeSizeText.text = shoe.size.toString()
+        }
 
         return binding.root
     }
