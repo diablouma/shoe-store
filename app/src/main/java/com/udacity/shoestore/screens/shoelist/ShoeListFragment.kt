8f1830e1eb2b50789c70e44627274ea4759ac20c
diablouma <mh732@hotmail.com>
2021-05-13 23:51:27 +0200

    Adds logout menun in listing screen

--- app/src/main/java/com/udacity/shoestore/screens/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoelist/ShoeListFragment.kt
@@ -1,11 +1,11 @@
 package com.udacity.shoestore.screens.shoelist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeListFragmentBinding
 
@@ -17,6 +17,19 @@ class ShoeListFragment : Fragment() {
     ): View? {
         val binding: ShoeListFragmentBinding =
             DataBindingUtil.inflate(inflater, R.layout.shoe_list_fragment, container, false)
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
