b9c4ff88824c937631ebc741cd485ed6e5aef10c
diablouma <mh732@hotmail.com>
2021-05-17 17:29:13 +0200

    inflates shoe detail fragment with data binding

--- app/src/main/java/com/udacity/shoestore/screens/shoedetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoedetail/ShoeDetailFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ShoeDetailFragmentBinding
 
 class ShoeDetailFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,8 @@ class ShoeDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val binding: ShoeDetailFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.shoe_detail_fragment, container, false)
+        return binding.root
     }
 }
