1b672c75a825ef1fd9cc423b24becbfb70827f74
diablouma <mh732@hotmail.com>
2021-05-18 09:14:28 +0200

    hides keyboard on login screen when clicking buttons

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -19,15 +21,23 @@ class LoginFragment : Fragment() {
             DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
 
         binding.signInButton.setOnClickListener {
+            hideKeyboard()
             Navigation.findNavController(it)
                 .navigate(R.id.action_login_destination_to_welcome_destination)
         }
 
         binding.signUpButton.setOnClickListener {
+            hideKeyboard()
             Navigation.findNavController(it)
                 .navigate(R.id.action_login_destination_to_welcome_destination)
         }
 
         return binding.root
     }
+
+    private fun hideKeyboard() {
+        val keyboard = activity?.getSystemService<InputMethodManager>()
+        view?.let { keyboard?.hideSoftInputFromWindow(view?.windowToken, 0) }
+    }
 }
+
